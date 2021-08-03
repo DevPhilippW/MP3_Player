b68b2c70ee3afa80e7f93f2c5206dccfad987086
Philipp Werner <p.wern.pw@gmail.com>
2021-08-03 16:03:48 +0200

    Einbinden der  taglib

--- MP3_Player/MP3_Player/Program.cs
+++ MP3_Player/MP3_Player/Program.cs
@@ -14,6 +14,9 @@ namespace MP3_Player
         [STAThread]
         static void Main()
         {
+            Song song = new Song();
+            song.ParseMP3(@"D:\01 - Wake Me Up.mp3");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
--- MP3_Player/MP3_Player/Song.cs
+++ MP3_Player/MP3_Player/Song.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagLib;
 
 namespace MP3_Player
 {
@@ -11,12 +13,26 @@ namespace MP3_Player
         private string title;
         private string interpret;
         private string filename;
+        private string pathname;
+        private string absPathName;
         private string path;
-        private int laenge;
+        private TimeSpan laenge;
 
         public void ShowSongInfo()
         {
 
+        }
+
+        public void ParseMP3(string songPath)
+        {
+            filename = Path.GetFileName(songPath);
+            pathname = Path.GetDirectoryName(songPath);
+            absPathName = songPath;
+
+            TagLib.File tagFile = TagLib.File.Create(songPath);
+            laenge = tagFile.Properties.Duration;
+            interpret = tagFile.Tag.JoinedAlbumArtists;
+            title = tagFile.Tag.Title;
         }
     }
 }
