27355c79549045697b4a049f54e188ad1faa9e5b
Philipp Werner <p.wern.pw@gmail.com>
2021-08-03 15:40:48 +0200

    Anlegen der Klasse Song

---
+++ MP3_Player/MP3_Player/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3_Player
+{
+    class Song
+    {
+        private string title;
+        private string interpret;
+        private string filename;
+        private string path;
+        private int laenge;
+
+        public void ShowSongInfo()
+        {
+
+        }
+    }
+}
